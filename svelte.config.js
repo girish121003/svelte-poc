e891e2bedbf2d7fc7897cb10edee777028a6bb6b
girish121003 <girish121003@gmail.com>
2025-03-05 14:04:07 -0500

    initial commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,7 +11,14 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'dist',  // Set the output folder to "dist"
+			assets: 'dist',
+			fallback: 'index.html' // Needed for single-page apps
+	}),
+		paths: {
+			base: "", // Set this if deploying to a subpath (e.g., GitHub Pages)
+		  }
 	}
 };
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+
 export default defineConfig({
-	plugins: [sveltekit()]
+	base:"/svelte_krogers_project/",
+	plugins: [sveltekit()],
+	build: {
+		outDir: 'dist' // Ensure this is set if you need a 'dist' folder
+	  }
 });
